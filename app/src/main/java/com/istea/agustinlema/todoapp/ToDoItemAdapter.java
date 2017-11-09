249fdc67211b08c580890168f1812b0c4b4120b7
agustin.lema <agustin.lema@istea.com.ar>
2017-11-09 00:20:12 -0300

    Created entity and adapter

---
+++ app/src/main/java/com/istea/agustinlema/todoapp/ToDoItemAdapter.java
@@ -0,0 +1,45 @@
+package com.istea.agustinlema.todoapp;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.istea.agustinlema.todoapp.model.ToDoItem;
+
+import java.util.List;
+
+public class ToDoItemAdapter extends ArrayAdapter<ToDoItem> {
+
+    public ToDoItemAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<ToDoItem> items) {
+        super(context, resource, items);
+    }
+
+    @NonNull
+    @Override
+    //Se ejecuta por cada elemento de la lista
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        ToDoItem item = getItem(position);
+
+        //Genero una vista en base a mi xml
+        View v = LayoutInflater.from(getContext()).inflate(R.layout.item_todoitem,null);
+
+        //Accedo al textview de la vista que acabo de generar
+        TextView tvTitle = v.findViewById(R.id.tvTitle);
+        TextView tvBody = v.findViewById(R.id.tvBody);
+        LinearLayout layoutItem = v.findViewById(R.id.layoutItem);
+        tvTitle.setText(item.getTitle());
+        tvBody.setText(item.getBody());
+        if (item.isImportant()) layoutItem.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.primary_light));
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/istea/agustinlema/todoapp/model/ToDoItem.java
@@ -0,0 +1,36 @@
+package com.istea.agustinlema.todoapp.model;
+
+public class ToDoItem {
+    private int id;
+    private String title;
+    private String body;
+    private boolean isImportant;
+
+    public ToDoItem(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public boolean isImportant() {
+        return isImportant;
+    }
+
+    public void setImportant(boolean important) {
+        isImportant = important;
+    }
+}
